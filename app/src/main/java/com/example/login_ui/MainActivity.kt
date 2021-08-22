bf8ee44854fd095711203759316237f9e9cd2f04
Lune <lune9402@naver.com>
2021-08-22 16:23:29 +0900

    로그인 토스트 추가

--- app/src/main/java/com/example/login_ui/MainActivity.kt
+++ app/src/main/java/com/example/login_ui/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.login_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val checkId = inputId.text.toString()
+            val checkPw = inputPw.text.toString()
+
+            if (checkId == "admin@test.com" && checkPw == "qwer") {
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
